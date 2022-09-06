9ab3ea823634229f6fa6f9ffbfcadba38470f4f6
CoderfishGzh <335247945@qq.com>
2022-09-06 15:57:31 +0800

    add sp-hamster in the primitives

---
+++ primitives/hamster/src/lib.rs
@@ -0,0 +1,33 @@
+mod constants;
+pub mod p_gateway;
+pub mod p_market;
+pub mod p_chunkcycle;
+pub mod p_provider;
+pub mod p_resource_order;
+
+use sp_runtime::{
+	traits::{IdentifyAccount, Verify},
+	MultiSignature,
+};
+
+/// An index to a block.
+pub type BlockNumber = u32;
+
+/// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
+pub type Signature = MultiSignature;
+
+/// Some way of identifying an account on the chain. We intentionally make it equivalent
+/// to the public key of our transaction signing scheme.
+pub type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+/// Balance of an account.
+pub type Balance = u128;
+
+/// Index of a transaction in the chain.
+pub type Index = u32;
+
+/// A hash of some data used by the chain.
+pub type Hash = sp_core::H256;
+
+/// Counter for the number of eras that have passed.
+pub type EraIndex = u32;
